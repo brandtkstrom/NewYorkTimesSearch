e18b6eb6d96bd7e2ebe84d61b2f05917fe688886
Nary Hang <naryhang@gmail.com>
2019-06-27 21:50:51 -0700

    adding API, query URL, function response

--- nytime.js
+++ nytime.js
@@ -1,1 +1,17 @@
+$("#search").on("click", function() {
 
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey+q6j9wM5GmujWbu8FZWzFc6qANAByuytF";
+        
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      .then(function(response) {
+
+        var text = response.data.text_original_url;
+        console.log(response);
+
+    });
+    });
