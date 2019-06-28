acaaf447e37df203fb97310ed42613708aa24684
Brandt Strom <brandtkstrom@gmail.com>
2019-06-28 13:36:22 -0700

    reorganize page elements into sections

--- nytime.js
+++ nytime.js
@@ -2,45 +2,45 @@ var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 var apiKey = "ALVr4fwE8asfGEipSTIuy4pJa88hg15Y";
 
 function getArticles(searchTerm) {
-  if (!searchTerm || searchTerm.trim() === "") {
-    // No search term provided -> return early
-    return;
-  }
+    if (!searchTerm || searchTerm.trim() === "") {
+        // No search term provided -> return early
+        return;
+    }
 
-  var queryURL = `${apiUrl}?q=${searchTerm}&api-key=${apiKey}`;
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(data) {
-    console.log(data);
-    var articles = data.response.docs;
-    printArticles(articles);
-  });
+    var queryURL = `${apiUrl}?q=${searchTerm}&api-key=${apiKey}`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(data) {
+        console.log(data);
+        var articles = data.response.docs;
+        printArticles(articles);
+    });
 }
 
 function printArticles(articles) {
-  $("#article-content").empty();
-  articles.forEach(article => {
-    var $headline = $("<p>").text(article.headline.main);
-    var $byline = $("<p>").text(`By: ${article.byline.original}`);
-    var $image = $("<img>").attr(
-      "src",
-      `http://nyt.com/${article.multimedia[0].url}`
-    );
+    $("#article-content").empty();
+    articles.forEach(article => {
+        var $headline = $("<p>").text(article.headline.main);
+        var $byline = $("<p>").text(`By: ${article.byline.original}`);
+        var $image = $("<img>").attr(
+            "src",
+            `http://nyt.com/${article.multimedia[0].url}`
+        );
 
-    var $article = $("<div>").append($headline, $byline, $image);
+        var $article = $("<div>").append($headline, $byline, $image);
 
-    $("#article-content").append($article);
-  });
+        $("#article-content").append($article);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  $("#search").on("click", function() {
-    var searchTerm = $("#term").val();
-    getArticles(searchTerm);
-  });
+    $("#search").on("click", function() {
+        var searchTerm = $("#term").val();
+        getArticles(searchTerm);
+    });
 
-  $("#clear").on("click", function() {
-    $("#article-content").empty();
-  });
+    $("#clear").on("click", function() {
+        $("#article-content").empty();
+    });
 });
