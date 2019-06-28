4ed09162bdd2cb6f3698022286a381634d8f79a2
Brandt Strom <brandtkstrom@gmail.com>
2019-06-28 16:05:14 -0700

    format retrieved articles as bootstrap cards

--- nytime.js
+++ nytime.js
@@ -1,6 +1,9 @@
-var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-var apiKey = "ALVr4fwE8asfGEipSTIuy4pJa88hg15Y";
+// Define constants to hold NY Times API address and API key.
+const apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const apiKey = "ALVr4fwE8asfGEipSTIuy4pJa88hg15Y";
 
+// Performs an article search via the NY Times API using the provided
+// arguments (search paramters).
 function getArticles(searchTerm, recordCt, startYear, endYear) {
     if (!searchTerm || searchTerm.trim() === "") {
         // No search term provided -> return early
@@ -27,33 +30,62 @@ function getArticles(searchTerm, recordCt, startYear, endYear) {
         url: queryURL,
         method: "GET"
     }).then(function(data) {
-        // Pull the returned articles out of the reponse and output to screen
+        // Log response to console
+        console.log(data);
+
+        // Pull the returned articles out of the reponse
         var articles = data.response.docs;
 
         if (recordCt != "all") {
             // Only return the specified article count
             articles = articles.slice(0, recordCt);
         }
+
+        // Output articles to screen
         printArticles(articles);
     });
 }
 
-// TODO - modify to print each article with correct formatting and content
+// Formats and outputs the provided article array to screen.
 function printArticles(articles) {
     $("#article-content").empty();
+
     articles.forEach(article => {
-        var $headline = $("<p>").text(article.headline.main);
-        var $byline = $("<p>").text(`By: ${article.byline.original}`);
-        var $image = $("<img>")
-            .attr("src", `http://nyt.com/${article.multimedia[0].url}`)
-            .css("max-width", "100%");
-
-        var $article = $("<div>").append($headline, $byline, $image);
-
-        $("#article-content").append($article);
+        var $card = getArticleCard(article);
+        $("#article-content").append($card);
     });
 }
 
+// Contructs and returns a Bootstrap card with content from the provided article object.
+function getArticleCard(article) {
+    var $card = $("<div>")
+        .addClass("card p-2 mb-4")
+        .css({ "max-width": "45%", "min-width": "250px" });
+    var $cardBody = $("<div>").addClass("card-body");
+    var $title = $("<h4>")
+        .addClass("card-title my-3")
+        .text(article.headline.main);
+    var $byline = $("<h5>")
+        .addClass("card-subtitle my-3 text-muted")
+        .text(`By: ${article.byline.original}`);
+    var $paragraph = $("<p>")
+        .addClass("card-text")
+        .text(article.lead_paragraph);
+    var $link = $("<a>")
+        .attr({ href: article.web_url, target: "_blank" })
+        .addClass("card-link")
+        .text("Read More");
+    var $image = $("<img>")
+        .attr("src", `https://nyt.com/${article.multimedia[0].url}`)
+        .css({ "object-fit": "cover", width: "100%", height: "30vh" });
+
+    $cardBody.append($image, $title, $byline, $paragraph, $link);
+    $card.append($cardBody);
+
+    return $card;
+}
+
+// Attach event listeners once DOM content is loaded.
 document.addEventListener("DOMContentLoaded", () => {
     // Click handler for search button
     $("#search").on("click", function() {
