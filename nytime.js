f51654142b09782690b1350d7d23a363f1992700
Brandt Strom <brandtkstrom@gmail.com>
2019-06-29 09:15:11 -0700

    trim search parameter input values

--- nytime.js
+++ nytime.js
@@ -1,11 +1,11 @@
 // Define constants to hold NY Times API address and API key.
-const apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-const apiKey = "ALVr4fwE8asfGEipSTIuy4pJa88hg15Y";
+const apiUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const apiKey = 'ALVr4fwE8asfGEipSTIuy4pJa88hg15Y';
 
 // Performs an article search via the NY Times API using the provided
 // arguments (search paramters).
 function getArticles(searchTerm, recordCt, startYear, endYear) {
-    if (!searchTerm || searchTerm.trim() === "") {
+    if (!searchTerm || searchTerm.trim() === '') {
         // No search term provided -> return early
         return;
     }
@@ -23,12 +23,12 @@ function getArticles(searchTerm, recordCt, startYear, endYear) {
     urlParams.push(`api-key=${apiKey}`);
 
     // Construct API request URL
-    var queryURL = `${apiUrl}?q=${searchTerm}&${urlParams.join("&")}`;
+    var queryURL = `${apiUrl}?q=${searchTerm}&${urlParams.join('&')}`;
 
     // Perform Ajax GET request to API
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: 'GET'
     }).then(function(data) {
         // Log response to console
         console.log(data);
@@ -36,7 +36,7 @@ function getArticles(searchTerm, recordCt, startYear, endYear) {
         // Pull the returned articles out of the reponse
         var articles = data.response.docs;
 
-        if (recordCt != "all") {
+        if (recordCt != 'all') {
             // Only return the specified article count
             articles = articles.slice(0, recordCt);
         }
@@ -48,44 +48,44 @@ function getArticles(searchTerm, recordCt, startYear, endYear) {
 
 // Formats and outputs the provided article array to screen.
 function printArticles(articles) {
-    $("#article-content").empty();
+    $('#article-content').empty();
 
     articles.forEach(article => {
         var $card = getArticleCard(article);
-        $("#article-content").append($card);
+        $('#article-content').append($card);
     });
 }
 
 // Contructs and returns a Bootstrap card with content from the provided article object.
 function getArticleCard(article) {
-    var $card = $("<div>")
-        .addClass("card article-card p-2 mb-4")
-        .css({ "min-width": "250px" });
-    var $cardBody = $("<div>").addClass("card-body");
-    var $title = $("<h4>")
-        .addClass("card-title my-3")
+    var $card = $('<div>')
+        .addClass('card article-card p-2 mb-4')
+        .css({ 'min-width': '250px' });
+    var $cardBody = $('<div>').addClass('card-body');
+    var $title = $('<h4>')
+        .addClass('card-title my-3')
         .text(article.headline.main);
     var author = article.byline.original
         ? article.byline.original
-        : "Unlisted Author";
-    var $byline = $("<h5>")
-        .addClass("card-subtitle my-3 text-muted")
+        : 'Unlisted Author';
+    var $byline = $('<h5>')
+        .addClass('card-subtitle my-3 text-muted')
         .text(`${author}`);
-    var $paragraph = $("<p>")
-        .addClass("card-text")
+    var $paragraph = $('<p>')
+        .addClass('card-text')
         .text(article.lead_paragraph);
-    var $link = $("<a>")
-        .attr({ href: article.web_url, target: "_blank" })
-        .addClass("card-link")
-        .text("Read More");
+    var $link = $('<a>')
+        .attr({ href: article.web_url, target: '_blank' })
+        .addClass('card-link')
+        .text('Read More');
 
-    var imgAddress = "images/nyt_default.png";
+    var imgAddress = 'images/nyt_default.png';
     if (article.multimedia.length > 0) {
         imgAddress = `https://nyt.com/${article.multimedia[0].url}`;
     }
-    var $image = $("<img>")
-        .attr("src", `${imgAddress}`)
-        .css({ "object-fit": "cover", width: "100%", height: "30vh" });
+    var $image = $('<img>')
+        .attr('src', `${imgAddress}`)
+        .css({ 'object-fit': 'cover', width: '100%', height: '30vh' });
 
     $cardBody.append($image, $title, $byline, $paragraph, $link);
     $card.append($cardBody);
@@ -94,25 +94,33 @@ function getArticleCard(article) {
 }
 
 // Attach event listeners once DOM content is loaded.
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     // Click handler for search button
-    $("#search").on("click", function() {
+    $('#search').on('click', function() {
         // Retreive search parameters from input fields
-        var searchTerm = $("#term").val();
-        var recordCt = $("#record-ct").val();
-        var startYear = $("#start-year").val();
-        var endYear = $("#end-year").val();
+        var searchTerm = $('#term')
+            .val()
+            .trim();
+        var recordCt = $('#record-ct')
+            .val()
+            .trim();
+        var startYear = $('#start-year')
+            .val()
+            .trim();
+        var endYear = $('#end-year')
+            .val()
+            .trim();
 
         // Pass search params to function that will perform the API request
         getArticles(searchTerm, recordCt, startYear, endYear);
     });
 
     // Click handler for clear button
-    $("#clear").on("click", function() {
-        $("#article-content").empty();
+    $('#clear').on('click', function() {
+        $('#article-content').empty();
     });
 
-    $("#search-form").submit(evt => {
+    $('#search-form').submit(evt => {
         evt.preventDefault();
     });
 });
